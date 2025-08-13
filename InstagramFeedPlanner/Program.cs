0fc7c3e1c4b7e3d0e80f06952aed305330331371
krzys2 <65448222+krzyssok2@users.noreply.github.com>
2025-08-13 15:50:10 +0300

    Initial with parts taken from previous displate planner project

--- InstagramFeedPlanner/Program.cs
+++ InstagramFeedPlanner/Program.cs
@@ -1,4 +1,6 @@
+using Blazored.LocalStorage;
 using InstagramFeedPlanner;
+using InstagramFeedPlanner.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorageAsSingleton();
+
+builder.Services.AddSingleton<ThemeService>();
 
 await builder.Build().RunAsync();
---
+++ InstagramFeedPlanner/Services/ThemeService.cs
@@ -0,0 +1,23 @@
+﻿namespace InstagramFeedPlanner.Services;
+
+public class ThemeService
+{
+    public event Action? OnThemeChanged;
+
+    private bool _isDarkMode;
+
+    public bool IsDarkMode
+    {
+        get => _isDarkMode;
+        set
+        {
+            if (_isDarkMode != value)
+            {
+                _isDarkMode = value;
+                OnThemeChanged?.Invoke();
+            }
+        }
+    }
+
+    public string CurrentTheme => IsDarkMode ? "dark" : "white";
+}
