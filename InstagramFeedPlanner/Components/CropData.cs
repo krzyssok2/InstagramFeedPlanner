8880b7746cb7eb0982b7be8515babffb735fc5b3
krzys2 <65448222+krzyssok2@users.noreply.github.com>
2025-08-15 21:35:55 +0300

    Initial Resizer

---
+++ InstagramFeedPlanner/Components/CropData.cs
@@ -0,0 +1,12 @@
+namespace InstagramFeedPlanner.Components
+{
+    public class CropData
+    {
+        public double PosX { get; set; }
+        public double PosY { get; set; }
+        public double Scale { get; set; }
+        public double ZoomValue { get; set; }
+        public double ImgWidth { get; set; }
+        public double ImgHeight { get; set; }
+    }
+}
